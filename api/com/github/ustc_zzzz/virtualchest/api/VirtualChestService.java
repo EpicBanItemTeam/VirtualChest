5f8c017244140f17a1ca4820d6f277fb09fbe09f
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2018-08-22 21:54:36 +0800

    Add more JavaDocs for VirtualChestService

--- api/com/github/ustc_zzzz/virtualchest/api/VirtualChestService.java
+++ api/com/github/ustc_zzzz/virtualchest/api/VirtualChestService.java
@@ -2,19 +2,22 @@ package com.github.ustc_zzzz.virtualchest.api;
 
 import com.github.ustc_zzzz.virtualchest.api.event.VirtualChestLoadEvent;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.service.ServiceManager;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 
 import java.util.Optional;
 import java.util.Set;
 
 /**
- * Representation of a provider for chest GUIs. The implementation may not be thread safe.
+ * Representation of a service provider for the chest GUIs. The implementation can be retrieved by
+ * {@link ServiceManager#provideUnchecked}. The implementation may not be thread safe.
  * <p>
  * The interface does not handle chest GUI registration. Please firstly register an event listener
  * for {@link VirtualChestLoadEvent} and then invoke {@link VirtualChestLoadEvent#register} method
  * to register all the chest GUIs.
  *
  * @author ustc_zzzz
+ * @see ServiceManager
  * @see VirtualChestLoadEvent
  */
 @NonnullByDefault
@@ -35,7 +38,7 @@ public interface VirtualChestService
      * Returns the id of the chest GUI marked as opened if available.
      *
      * @param player the player
-     * @return the id or {@link Optional#empty()}
+     * @return the id or {@link Optional#empty}
      */
     Optional<String> lookup(Player player);
 
@@ -44,7 +47,7 @@ public interface VirtualChestService
      *
      * @param identifier the id for the chest GUI to be opened
      * @param player     the player
-     * @return true if the chest GUI is available and not opened, false otherwise
+     * @return true if the chest GUI is available and marked to be opened, false otherwise
      */
     boolean open(String identifier, Player player);
 
@@ -53,7 +56,7 @@ public interface VirtualChestService
      *
      * @param identifier the id for the chest GUI to be opened
      * @param player     the player
-     * @return true if the chest GUI is available and opened, false otherwise
+     * @return true if the chest GUI is available and mark to be closed, false otherwise
      */
     boolean close(String identifier, Player player);
 }
